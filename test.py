9fcb2633143b3bc8b632de8bf50ca65c29633272
DEV <96439384+DEV-SHUKLA-GITHUB@users.noreply.github.com>
2024-08-26 17:13:02 +0530

    cleaed project

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from jugaad_trader import Zerodha
-kite = Zerodha()
-instruments = pd.DataFrame(kite.instruments())
-print(instruments)
-print(set(instruments[(instruments['segment'] =='NFO-OPT') & (instruments['name'] =='NIFTY')].expiry))
